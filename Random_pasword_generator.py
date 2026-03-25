9c239d2712631151ea7b8da5396156a11ce3ac9f
Jozhua1 <joshuaescalona.on@gmail.com>
2026-03-25 13:39:22 +0800

    Initial commit

---
+++ Random_pasword_generator.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+def generate_password(length):
+    # Limit max length
+    if length > 69:
+        length = 69
+
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+    return "".join(random.choice(characters) for _ in range(length))
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    password = ""
+
+    if request.method == "POST":
+        length = int(request.form["length"])
+        password = generate_password(length)
+
+    return render_template("index.html", password=password)
+
+if __name__ == "__main__":
+    app.run(debug=True)
